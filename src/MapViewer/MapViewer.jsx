130f1040757e07a29d5a1e99f467ba376d18e332
gonzalezsd <gonzalezsd@gmail.com>
2021-05-24 09:43:19 +0200

    Comments to initial reboot

--- src/MapViewer/MapViewer.jsx
+++ src/MapViewer/MapViewer.jsx
@@ -1,34 +1,45 @@
 import React, { useRef, useEffect, useState, useCallback, createRef } from "react";
 import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
-import BasemapGallery from "@arcgis/core/widgets/BasemapGallery";
 import Zoom from "@arcgis/core/widgets/Zoom";
-import ScaleBar from "@arcgis/core/widgets/ScaleBar";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 import "@arcgis/core/assets/esri/css/main.css";
 import "./ArcgisMap.css";
-import ReactDOM from 'react-dom';
 import BasemapWidget from './BasemapWidget';
 
 class MapViewer extends React.Component {
+    /**
+     * This method does the creation of the main component 
+     * @param {*} props 
+     */
     constructor(props) {
         super(props);
-        this.mpd = createRef();
+        //we create a reference to the DOM element that will
+        //be later mounted. We will use the reference that we
+        //create here to reference the DOM element from javascript
+        //code, for example, to create later a MapView component 
+        //that will use the map div to show the map
+        this.mapdiv = createRef();
         this.mapCfg = props.cfg.Map;
         this.map = new Map({
             basemap: "topo-vector"
         });
     }
     
+    /**
+     * Once the component has been mounted in the screen, this method
+     * will be executed, so we can access to the DOM elements, since
+     * they are already mounted
+     */
     componentDidMount() {
+        // this.mapdiv.current is the reference to the current DOM element of 
+        // this.mapdiv after it was mounted by the render() method
         this.view = new MapView({
-                                    container: this.mpd.current,
+                                    container: this.mapdiv.current,
                                     map: this.map,
                                     center: this.mapCfg.center,
                                     zoom: this.mapCfg.zoom,
                                     ui: {
-                                        components: ["attribution"] // empty the UI, except for attribution
+                                        components: ["attribution"] 
                                     }
                                 });
         this.zoom = new Zoom({
@@ -37,18 +48,36 @@ class MapViewer extends React.Component {
         this.view.ui.add(this.zoom, {
             position: "top-right"
         });
+
+        //Once we have created the MapView, we need to ensure that the map div
+        //is refreshed in order to show the map on it. To do so, we need to
+        //trigger the renderization again, and to trigger the renderization
+        //we invoke the setState method, that changes the state and forces a
+        //react component to render itself again
         this.setState({});
     }
 
+    /**
+     * This method evaluates the ability to render the basemaps widget and
+     * returns the jsx allowing such a render (if conditions are ok)
+     * @returns jsx
+     */
     renderBasemap() {
         if (this.view)
             return <BasemapWidget view={this.view} />
     }
     
+    /**
+     * This method renders the map viewer, invoking if necessary the methods
+     * to render the other widgets to display
+     * @returns jsx
+     */
     render(){
+        // we use a reference (ref={this.mapdiv}) in order to reference a
+        // DOM element to be mounted (but not yet mounted)
         return(
             <div className="map-container">
-                <div ref={this.mpd} className="map">
+                <div ref={this.mapdiv} className="map">
                     {this.renderBasemap()}
                 </div>
             </div>
