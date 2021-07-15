788af33229a6f745dd554cdbd63af779b48da7c8
iaguerri <ireneaguerri@gmail.com>
2021-07-15 13:25:42 +0200

    WMSLayer for FeatureLayer highlighted

--- src/MapViewer/AreaWidget.jsx
+++ src/MapViewer/AreaWidget.jsx
@@ -1,10 +1,12 @@
 import Graphic from "@arcgis/core/Graphic";
 import Extent from "@arcgis/core/geometry/Extent";
-import WMSLayer from "@arcgis/core/layers/WMSLayer";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+// import WMSLayer from "@arcgis/core/layers/WMSLayer";
 import GroupLayer from "@arcgis/core/layers/GroupLayer";
 import React, { createRef } from "react";
 import "@arcgis/core/assets/esri/css/main.css";
 import "./ArcgisMap.css";
+import { layer } from "@fortawesome/fontawesome-svg-core";
 
 class AreaWidget extends React.Component {
     /**
@@ -17,8 +19,11 @@ class AreaWidget extends React.Component {
         this.container = createRef();
         //Initially, we set the state of the component to 
         //not be showing the basemap panel
-        this.state = {showMapMenu: false};
+        this.state = { showMapMenu: false };
         this.menuClass = 'esri-icon-cursor-marquee esri-widget--button esri-widget esri-interactive';
+
+
+
     }
     /**
      * Method that will be invoked when the 
@@ -29,25 +34,25 @@ class AreaWidget extends React.Component {
         if (this.state.showMapMenu) {
             this.props.mapViewer.setActiveWidget();
             this.container.current.querySelector(".area-panel").style.display = 'none';
-            this.container.current.querySelector(".esri-widget--button").classList.replace('esri-icon-right-arrow','esri-icon-cursor-marquee');
+            this.container.current.querySelector(".esri-widget--button").classList.replace('esri-icon-right-arrow', 'esri-icon-cursor-marquee');
             // By invoking the setState, we notify the state we want to reach
             // and ensure that the component is rendered again
-            this.setState({showMapMenu: false});
+            this.setState({ showMapMenu: false });
             this.clearWidget();
-            this.container.current.querySelector("#download_area_select_nuts0").checked=true;
+            this.container.current.querySelector("#download_area_select_nuts0").checked = true;
 
         } else {
             this.props.mapViewer.setActiveWidget(this);
             this.container.current.querySelector(".area-panel").style.display = 'block';
-            this.container.current.querySelector(".esri-widget--button").classList.replace('esri-icon-cursor-marquee','esri-icon-right-arrow');
+            this.container.current.querySelector(".esri-widget--button").classList.replace('esri-icon-cursor-marquee', 'esri-icon-right-arrow');
             // By invoking the setState, we notify the state we want to reach
             // and ensure that the component is rendered again
-            this.setState({showMapMenu: true});
+            this.setState({ showMapMenu: true });
             this.container.current.querySelector("input:checked").click()
         }
     };
     nuts0handler(e) {
-        this.loadNutsService(e.target.value, 0)
+        this.loadNutsService(e.target.value, 0)    
     }
     nuts1handler(e) {
         this.loadNutsService(e.target.value, 1)
@@ -55,68 +60,60 @@ class AreaWidget extends React.Component {
     nuts3handler(e) {
         this.loadNutsService(e.target.value, 3)
     }
+
     loadNutsService(id, level) {
         this.clearWidget();
-        var url = "https://bm-eugis.tk/arcgis/services/CLMS/NUTS_2021/MapServer/WmsServer?"
-        var layer = new WMSLayer({
-          url: url,
-          id: id,
-          featureInfoFormat: "text/html",
-          featureInfoUrl: url,
-          sublayers: [
-            {
-              name: 0,
-              popupEnabled: true,
-              queryable: true
-            }
-          ],
-          customParameters: {
-            layerDefs: "{'0':'LEVL_CODE="+ level +"'}",
-          },
-          
+        var url = "https://bm-eugis.tk/arcgis/rest/services/CLMS/NUTS_2021/MapServer/0"
+        var layer = new FeatureLayer({
+            url: url,
+            id: id,
+            outFields: ["*"],
+            popupEnabled: true,
+            definitionExpression: "LEVL_CODE=" + level
         });
+
         this.nutsGroupLayer.add(layer);
     }
     rectanglehandler() {
         this.clearWidget();
         var fillSymbol = {
             type: "simple-fill",
-            color: [255,255,255,0.5],
+            color: [255, 255, 255, 0.5],
             outline: {
-              color: [0, 0, 0],
-              width: 1
+                color: [0, 0, 0],
+                width: 1
             }
         };
-          
+
         let extentGraphic = null;
         let origin = null;
         const drawGraphics = this.props.view.on('drag', e => {
             e.stopPropagation();
-            if (e.action === 'start'){
+            if (e.action === 'start') {
                 if (extentGraphic) this.props.view.graphics.remove(extentGraphic)
                 origin = this.props.view.toMap(e);
-            } else if (e.action === 'update'){
+            } else if (e.action === 'update') {
                 if (extentGraphic) this.props.view.graphics.remove(extentGraphic)
-                let p = this.props.view.toMap(e); 
+                let p = this.props.view.toMap(e);
                 extentGraphic = new Graphic({
-                geometry: new Extent({
-                    xmin: Math.min(p.x, origin.x),
-                    xmax: Math.max(p.x, origin.x),
-                    ymin: Math.min(p.y, origin.y),
-                    ymax: Math.max(p.y, origin.y),
-                    spatialReference: { wkid: 102100 }
-                }),
-                symbol: fillSymbol
+                    geometry: new Extent({
+                        xmin: Math.min(p.x, origin.x),
+                        xmax: Math.max(p.x, origin.x),
+                        ymin: Math.min(p.y, origin.y),
+                        ymax: Math.max(p.y, origin.y),
+                        spatialReference: { wkid: 102100 }
+                    }),
+                    symbol: fillSymbol
                 })
                 this.props.view.graphics.add(extentGraphic)
             }
         });
-        this.setState({ShowGraphics:drawGraphics});
+        this.setState({ ShowGraphics: drawGraphics });
     }
     clearWidget() {
         if (this.state.ShowGraphics) {
             this.state.ShowGraphics.remove();
-            this.setState({ShowGraphics:null});
+            this.setState({ ShowGraphics: null });
         }
         this.nutsGroupLayer.removeAll();
         this.props.view.graphics.removeAll();
@@ -136,8 +133,8 @@ class AreaWidget extends React.Component {
      * This method renders the component
      * @returns jsx
      */
-    render(){
-        return(
+    render() {
+        return (
             <>
                 <div ref={this.container} className="area-container">
                     <div
